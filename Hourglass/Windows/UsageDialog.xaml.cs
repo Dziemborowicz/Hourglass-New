8988b921db98d92f2e6f085fbe9e6340a2967cc7
Ivan Ivon <ivan.ivon@gmail.com>
2024-10-26 13:04:21 +0300

    Suppress exception when running non-elevated Hourglass instance receives request from elevated Hourglass instance

--- Hourglass/Windows/UsageDialog.xaml.cs
+++ Hourglass/Windows/UsageDialog.xaml.cs
@@ -7,6 +7,7 @@
 namespace Hourglass.Windows;
 
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Navigation;
@@ -58,13 +59,21 @@ public sealed partial class UsageDialog
             ErrorMessage = errorMessage
         };
 
-        if (Application.Current?.Dispatcher is null)
+        try
         {
-            _instance.ShowDialog();
+            if (Application.Current?.Dispatcher is null)
+            {
+                _instance.ShowDialog();
+            }
+            else
+            {
+                _instance.Show();
+            }
         }
-        else
+        catch (COMException ex) when((uint)ex.ErrorCode == 0x80070542)
         {
-            _instance.Show();
+            // Thrown when running non-elevated Hourglass instance receives request from elevated Hourglass instance.
+            MessageBox.Show(ex.Message, Properties.Resources.AppName, MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 
