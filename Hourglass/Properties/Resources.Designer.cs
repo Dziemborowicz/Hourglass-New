e2911271badc52e20bce39e4717d7584d6070ae5
Ivan Ivon <ivan.ivon@gmail.com>
2025-01-02 21:15:23 +0200

    Update save this timer on close text

--- Hourglass/Properties/Resources.Designer.cs
+++ Hourglass/Properties/Resources.Designer.cs
@@ -1139,11 +1139,11 @@ namespace Hourglass.Properties {
         /// <summary>
         ///   Looks up a localized string similar to MIT License
         ///
-        ///Copyright © 2021 Chris Dziemborowicz, 2024 Ivan Ivon
+        ///Copyright © 2021 Chris Dziemborowicz, 2024-2025 Ivan Ivon
         ///
         ///Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
         ///
-        ///The a [rest of string was truncated]&quot;;.
+        /// [rest of string was truncated]&quot;;.
         /// </summary>
         public static string License {
             get {
@@ -1643,7 +1643,7 @@ namespace Hourglass.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &amp;Save this timer.
+        ///   Looks up a localized string similar to Sa&amp;ve this timer on close.
         /// </summary>
         public static string SaveTimerTaskDialogText {
             get {
